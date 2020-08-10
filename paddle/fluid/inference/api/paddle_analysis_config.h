08cb30d176a3c6a493a7bd3cda7852774a8f7926
wozna <joanna.wozna@intel.com>
2020-08-10 12:36:54 +0200

    Add mkldnn bfloat16 option to C-API

--- paddle/fluid/inference/api/paddle_analysis_config.h
+++ paddle/fluid/inference/api/paddle_analysis_config.h
@@ -402,6 +402,12 @@ struct PD_INFER_DECL AnalysisConfig {
   void EnableMkldnnQuantizer();
 
   ///
+  /// \brief Turn on MKLDNN bfloat16.
+  ///
+  ///
+  void EnableMkldnnBfloat16();
+
+  ///
   /// \brief A boolean state telling whether the thread local CUDA stream is
   /// enabled.
   ///
@@ -592,6 +598,7 @@ struct PD_INFER_DECL AnalysisConfig {
   int mkldnn_cache_capacity_{0};
   bool use_mkldnn_quantizer_{false};
   std::shared_ptr<MkldnnQuantizerConfig> mkldnn_quantizer_config_;
+  bool use_mkldnn_bfloat16_{false};
 
   // If the config is already used on a predictor, it becomes invalid.
   // Any config can only be used with one predictor.
--- paddle/fluid/inference/capi/pd_config.cc
+++ paddle/fluid/inference/capi/pd_config.cc
@@ -202,6 +202,11 @@ void PD_EnableMkldnnQuantizer(PD_AnalysisConfig* config) {
   config->config.EnableMkldnnQuantizer();
 }
 
+void PD_EnableMkldnnBfloat16(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
+  config->config.EnableMkldnnBfloat16();
+}
+
 bool PD_MkldnnQuantizerEnabled(const PD_AnalysisConfig* config) {
   PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.mkldnn_quantizer_enabled();
