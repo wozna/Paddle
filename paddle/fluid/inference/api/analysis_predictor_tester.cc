efd3b14ae96d1b0a5864021a3519ad320688075a
wozna <joanna.wozna@intel.com>
2021-06-24 14:59:02 +0200

    Remove mkldnn only

--- paddle/fluid/inference/api/analysis_predictor_tester.cc
+++ paddle/fluid/inference/api/analysis_predictor_tester.cc
@@ -250,7 +250,6 @@ TEST(AnalysisPredictor, memory_optim) {
 }
 */
 
-#ifdef PADDLE_WITH_MKLDNN
 class MkldnnQuantizerTest : public testing::Test {
  public:
   MkldnnQuantizerTest() {
@@ -526,8 +525,6 @@ TEST_F(MkldnnQuantizerTest, max_ch_gru_scaling_factor) {
     ASSERT_NEAR(lod_tensor.data<double>()[i], scales[i], abs_error);
   }
 }
-
-#endif
 
 #if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
 TEST(AnalysisPredictor, bf16_gpu_pass_strategy) {
